50e65eb4684e8a10393d51aa002fa16ed03de60a
Rafid Al Raiyan <146878173+Rafid-Al-Raiyan@users.noreply.github.com>
2024-04-22 15:32:24 +0600

    Add files via upload

---
+++ simple_division_code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+
+{
+
+    int a = 20, b = 5, result;
+
+    result = a/b;
+
+    printf("The result is %d", result);
+
+    return 0;
+
+}
---
+++ simple_multiplication_code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+
+{
+
+    int a = 50, b = 60, result;
+
+    result = a * b;
+
+    printf("The result is %d", result);
+
+    return 0;
+
+}
